5eb25a6d32b55abf92d4c7da368f185196f3dab0
TonyNutanix <tony.hughes@nutanix.com>
2022-12-28 15:56:12 -0800

    Initial Creation

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # Getting_Started_With_Terraform_and_Nutanix
-Easy to use starting point for learning Terraform and Nutanix
+
+## What you need to get started:
+1. A Nutanix Cluster running AHV and Prism Central
+2. A subnet named "Primary"
+
+## Here is what it does:
+1. Uploads a CentOS7 image
+2. Builds a VM
+3. Runs cloudinit script to setup the following:
+    - git
+    - terraform
+    - download the terraform scripts
+
+## How to use these files:
+1. Login to the CentoOS VM that was created
+2. ? CD to some directory?
+3. At the command prompt run 'terraform init'.  This will download the provider to your machine.
+4. Run 'terrform plan'
+    It should list a bunch of stuff with this at the end:
+    
+    Plan: 3 to add, 0 to change, 0 to destroy.
+
+4. Run 'terraform apply -auto-approve'
+    Sit back and watch everything get built in Prism Central.
+    In Prism Central:
+        Compute & Storage -> Images: Look for the "Terraform-CentOS7" image
+        Comptue & Storage -> VMs: Look for "terraform-vm".  The console login is user:root password: nutanix/4u
+
+NOTE: There may be a time gap between the time all items are built in Prism Central and the time Terraform thinks it is done.
+    Give it a while, it will eventually figure it out.  This can take as long as 30 minutes.
+
+5. Run 'terraform destroy -auto-approve' and everthing created by Terraform will be removed.  
+    Reconfirm by looking for the entities in Prism Central and they should no longer be present:
+        Compute & Storage -> Images: Look for the "Terraform-CentOS7" image
+        Comptue & Storage -> VMs: Look for "terraform-vm".  
+    This should run much faster than the "apply".
+
+Nice work!  The examples in the github repo are a great next step now that you have the basics figured out.
+
+## Other Notes
+
+This was created by Tony Hughes (aka TH in the comments) to provide a quick introduction to using Terraform with Nutanix in less than 100 lines of code.
---
+++ main.tf
@@ -0,0 +1,121 @@
+/*
+TH: This section tells Terraform which provider and version to be used.
+*/
+
+terraform {
+  required_providers {
+    nutanix = {
+      source  = "nutanix/nutanix"
+      version = "1.7.0"
+    }
+  }
+}
+
+/*
+TH: Update this section to reflect the proper information for the HPOC being used.
+
+username: Should not need to be changed
+password: Update to match the password of the HPOC
+port: Should not need to be changed
+endpoint: IP address for the Prism Central (this does not work if pointed to Prism Element)
+foundation_endpoint: Set it to the IP address of Prism Central. Foundation is not used in this example.  It is included simply to prevent Terraform from presenting a warning which may cause confusion
+fountation_port: No change needed.  Same as the foundation_endpoint, this is simply populated to prevent a warning.
+insecure: No change needed.
+wait_timeout: No change needed.
+
+*/
+provider "nutanix" {
+  username            = "admin"
+  password            = "nx2Tech590!"
+  port                = 9440
+  endpoint            = "10.38.46.39"
+  foundation_endpoint = "10.38.46.39"
+  foundation_port     = 8000
+  insecure            = true
+  wait_timeout        = 30
+}
+
+/*
+TH: This is the prefix that will be added to the created VMs.  Replace this with your initials.
+*/
+locals {
+    prefix_for_created_entities = "XYZ"
+}
+
+#################################################################################
+#
+#
+#  NOTHING BELOW HERE SHOULD REQUIRE MODIFICATION
+#
+#
+#################################################################################
+
+/*
+TH: Create a data source for Nutanix Clusters
+*/
+data "nutanix_clusters" "clusters" {
+}
+
+/*
+TH: Create a data source for the "Primary" subnet
+*/
+data "nutanix_subnet" "Primary" {
+    subnet_name = "Primary"
+}
+
+/*
+TH: This grabs a list of all clusters attached to the Prism Central, which includes the Prism Central itself.
+
+Change the "0" to "1" if the following error is presented during terraform plan or terraform apply 
+
+    Error: error: {
+   "api_version": "3.1",
+   "code": 400,
+   "message_list": [
+     {
+       "message": "Given input is invalid. Referenced cluster ddbde545-2546-4f44-9c8f-c3c722d495cc is not connected",
+       "reason": "INVALID_ARGUMENT"
+     }
+*/
+locals {
+  cluster1 = data.nutanix_clusters.clusters.entities[0].metadata.uuid
+}
+
+/*
+TH: Define the location for the image to be used for creating the VM.
+*/
+resource "nutanix_image" "Terraform-CentOS7" {
+  name        = "${var.prefix_for_created_entities}-Terraform-CentOS7"
+  # source_uri  = "http://10.42.194.11/workshop_staging/CentOS7.qcow2"
+  source_uri  = "https://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud.qcow2"
+  description = "${var.prefix_for_created_entities} Terraform-CentOS7 qcow image"
+}
+
+/*
+TH: This section contains all the details related to the VMs being built.
+*/
+resource "nutanix_virtual_machine" "terraform-vm" {
+  count = 10
+  name                 = "${var.prefix_for_created_entities}terraform-vm_${count.index}"
+  description          = "${var.prefix_for_created_entities}terraform-vm_${count.index}"
+  num_vcpus_per_socket = 2
+  num_sockets          = 1
+  memory_size_mib      = 4096
+  cluster_uuid = local.cluster1
+  nic_list {
+    subnet_uuid = data.nutanix_subnet.Primary.id 
+  }
+  disk_list {
+    data_source_reference = {
+      kind = "image"
+      uuid = nutanix_image.Terraform-CentOS7.id
+    }
+    device_properties {
+      disk_address = {
+        device_index = 0
+        adapter_type = "SCSI"
+      }
+      device_type = "DISK"
+    }
+  }
+}
