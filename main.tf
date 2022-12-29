93996f1232f887d606b8fb51145eff5be47a7499
TonyNutanix <tony.hughes@nutanix.com>
2022-12-28 16:44:27 -0800

    main changes

--- main.tf
+++ main.tf
@@ -28,8 +28,8 @@ provider "nutanix" {
   username            = "admin"
   password            = "nx2Tech714!"
   port                = 9440
-  endpoint            = "10.38.46.39"
-  foundation_endpoint = "10.38.46.39"
+  endpoint            = "10.38.3.137"
+  foundation_endpoint = "10.38.3.137"
   foundation_port     = 8000
   insecure            = true
   wait_timeout        = 30
